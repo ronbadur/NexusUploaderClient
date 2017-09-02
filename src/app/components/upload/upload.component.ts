17686df610c0519fb6aa48ff37a8002a52ae2624
Ron Badur <ronbadur02@gmail.com>
2017-09-03 01:40:46 +0300

    Initial Commit

---
+++ src/app/components/upload/upload.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class UploadComponent {
+  title = 'app';
+}
